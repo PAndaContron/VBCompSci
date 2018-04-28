6e202094a87ffdbb6a649d3e9390550f1ace803a
PAndaContron <rajtel99@gmail.com>
2018-04-28 18:51:00 -0400

    Added Prog3_5c

---
+++ Prog3_5c/Prog3_5c/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub mnuAssoc_Click(sender As Object, e As EventArgs) Handles mnuAssoc.Click
+        lblProperty.Text = "(a + b) + c = a + (b + c)"
+    End Sub
+
+    Private Sub mnuComm_Click(sender As Object, e As EventArgs) Handles mnuComm.Click
+        lblProperty.Text = "a + b = b + a"
+    End Sub
+
+    Private Sub mnuExit_Click(sender As Object, e As EventArgs) Handles mnuExit.Click
+        End
+    End Sub
+End Class
