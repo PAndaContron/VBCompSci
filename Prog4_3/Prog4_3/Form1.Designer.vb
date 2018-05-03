4587a7cf7b4344e1cddfb394babbbd710933ec67
ContronPAnda <rajtel99@gmail.com>
2018-05-03 12:54:32 -0400

    Finished Prog4_3

--- Prog4_3/Prog4_3/Form1.Designer.vb
+++ Prog4_3/Prog4_3/Form1.Designer.vb
@@ -22,110 +22,131 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblLength = New System.Windows.Forms.Label()
+        Me.lblWidth = New System.Windows.Forms.Label()
+        Me.lblArea = New System.Windows.Forms.Label()
+        Me.lblPerimeter = New System.Windows.Forms.Label()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtWidth = New System.Windows.Forms.TextBox()
+        Me.txtLength = New System.Windows.Forms.TextBox()
+        Me.btnAnswer = New System.Windows.Forms.Button()
+        Me.mnuProgram = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'Label1
+        'lblLength
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(0, 0)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Label1"
+        Me.lblLength.AutoSize = True
+        Me.lblLength.Location = New System.Drawing.Point(55, 62)
+        Me.lblLength.Name = "lblLength"
+        Me.lblLength.Size = New System.Drawing.Size(43, 13)
+        Me.lblLength.TabIndex = 0
+        Me.lblLength.Text = "Length:"
         '
-        'Label2
+        'lblWidth
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(8, 8)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(39, 13)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Label2"
+        Me.lblWidth.AutoSize = True
+        Me.lblWidth.Location = New System.Drawing.Point(213, 62)
+        Me.lblWidth.Name = "lblWidth"
+        Me.lblWidth.Size = New System.Drawing.Size(38, 13)
+        Me.lblWidth.TabIndex = 1
+        Me.lblWidth.Text = "Width:"
         '
-        'Label3
+        'lblArea
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(16, 16)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(39, 13)
-        Me.Label3.TabIndex = 2
-        Me.Label3.Text = "Label3"
+        Me.lblArea.AutoSize = True
+        Me.lblArea.Location = New System.Drawing.Point(16, 135)
+        Me.lblArea.Name = "lblArea"
+        Me.lblArea.Size = New System.Drawing.Size(38, 13)
+        Me.lblArea.TabIndex = 2
+        Me.lblArea.Text = "Area ="
         '
-        'Label4
+        'lblPerimeter
         '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(24, 24)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(39, 13)
-        Me.Label4.TabIndex = 3
-        Me.Label4.Text = "Label4"
+        Me.lblPerimeter.AutoSize = True
+        Me.lblPerimeter.Location = New System.Drawing.Point(16, 166)
+        Me.lblPerimeter.Name = "lblPerimeter"
+        Me.lblPerimeter.Size = New System.Drawing.Size(60, 13)
+        Me.lblPerimeter.TabIndex = 3
+        Me.lblPerimeter.Text = "Perimeter ="
         '
         'MenuStrip1
         '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuProgram})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(284, 24)
+        Me.MenuStrip1.Size = New System.Drawing.Size(340, 24)
         Me.MenuStrip1.TabIndex = 4
         Me.MenuStrip1.Text = "MenuStrip1"
         '
-        'TextBox1
+        'txtWidth
         '
-        Me.TextBox1.Location = New System.Drawing.Point(8, 8)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox1.TabIndex = 5
+        Me.txtWidth.Location = New System.Drawing.Point(257, 59)
+        Me.txtWidth.Name = "txtWidth"
+        Me.txtWidth.Size = New System.Drawing.Size(48, 20)
+        Me.txtWidth.TabIndex = 5
         '
-        'TextBox2
+        'txtLength
         '
-        Me.TextBox2.Location = New System.Drawing.Point(16, 16)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox2.TabIndex = 6
+        Me.txtLength.Location = New System.Drawing.Point(104, 59)
+        Me.txtLength.Name = "txtLength"
+        Me.txtLength.Size = New System.Drawing.Size(48, 20)
+        Me.txtLength.TabIndex = 6
         '
-        'Button1
+        'btnAnswer
         '
-        Me.Button1.Location = New System.Drawing.Point(24, 24)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 7
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnAnswer.Location = New System.Drawing.Point(243, 156)
+        Me.btnAnswer.Name = "btnAnswer"
+        Me.btnAnswer.Size = New System.Drawing.Size(75, 23)
+        Me.btnAnswer.TabIndex = 7
+        Me.btnAnswer.Text = "Answer"
+        Me.btnAnswer.UseVisualStyleBackColor = True
+        '
+        'mnuProgram
+        '
+        Me.mnuProgram.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuExit})
+        Me.mnuProgram.Name = "mnuProgram"
+        Me.mnuProgram.Size = New System.Drawing.Size(65, 20)
+        Me.mnuProgram.Text = "Program"
+        '
+        'mnuExit
+        '
+        Me.mnuExit.Name = "mnuExit"
+        Me.mnuExit.Size = New System.Drawing.Size(152, 22)
+        Me.mnuExit.Text = "Exit"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
+        Me.ClientSize = New System.Drawing.Size(340, 210)
+        Me.Controls.Add(Me.btnAnswer)
+        Me.Controls.Add(Me.txtLength)
+        Me.Controls.Add(Me.txtWidth)
+        Me.Controls.Add(Me.lblPerimeter)
+        Me.Controls.Add(Me.lblArea)
+        Me.Controls.Add(Me.lblWidth)
+        Me.Controls.Add(Me.lblLength)
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Label1 As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Label3 As Label
-    Friend WithEvents Label4 As Label
+    Friend WithEvents lblLength As Label
+    Friend WithEvents lblWidth As Label
+    Friend WithEvents lblArea As Label
+    Friend WithEvents lblPerimeter As Label
     Friend WithEvents MenuStrip1 As MenuStrip
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents Button1 As Button
+    Friend WithEvents txtWidth As TextBox
+    Friend WithEvents txtLength As TextBox
+    Friend WithEvents btnAnswer As Button
+    Friend WithEvents mnuProgram As ToolStripMenuItem
+    Friend WithEvents mnuExit As ToolStripMenuItem
 End Class
--- Prog4_3/Prog4_3/Form1.vb
+++ Prog4_3/Prog4_3/Form1.vb
@@ -1,3 +1,13 @@
 ﻿Public Class Form1
+    Private Sub btnAnswer_Click(sender As Object, e As EventArgs) Handles btnAnswer.Click
+        Dim dblLength As Double = Val(txtLength.Text)
+        Dim dblWidth As Double = Val(txtWidth.Text)
 
+        lblArea.Text = "Area = " + Str(dblLength * dblWidth)
+        lblPerimeter.Text = "Perimeter = " + Str(2 * (dblLength + dblWidth))
+    End Sub
+
+    Private Sub mnuExit_Click(sender As Object, e As EventArgs) Handles mnuExit.Click
+        End
+    End Sub
 End Class
