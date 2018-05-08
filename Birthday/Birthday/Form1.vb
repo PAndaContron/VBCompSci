2d7b4ebd58a68b6d3d2bbe493ba299332dcc46ad
ContronPAnda <rajtel99@gmail.com>
2018-05-08 13:13:27 -0400

    Created Birthday Program

---
+++ Birthday/Birthday/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For intPeople = 1 To 365
+            Dim 
+            For intDay = 365 To 366 - intPeople
+
+            Next
+            lstList.Items.Add(Str(intPeople))
+        Next
+    End Sub
+End Class
