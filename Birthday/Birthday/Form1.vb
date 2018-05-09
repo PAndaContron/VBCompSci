a90af8c0c4f1f406f39179205729738a1231b5e0
ContronPAnda <rajtel99@gmail.com>
2018-05-09 13:13:21 -0400

    Did some stuff

--- Birthday/Birthday/Form1.vb
+++ Birthday/Birthday/Form1.vb
@@ -1,11 +1,12 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         For intPeople = 1 To 365
-            Dim 
+            Dim decChance As Decimal = 1
             For intDay = 365 To 366 - intPeople
-
+                decChance *= intDay / 365
             Next
-            lstList.Items.Add(Str(intPeople))
+            decChance = 1 - decChance
+            lstList.Items.Add(Str(intPeople) + " People: " & Format(Str(decChance), "Percent"))
         Next
     End Sub
 End Class
