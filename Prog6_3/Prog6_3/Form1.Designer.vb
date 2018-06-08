5d7d1893d1b3cbb2ce7b04019bc62b8fc4e9da6a
ContronPAnda <rajtel99@gmail.com>
2018-06-08 13:10:26 -0400

    Added Prog6_3

---
+++ Prog6_3/Prog6_3/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblStart = New System.Windows.Forms.Label()
+        Me.lblEnd = New System.Windows.Forms.Label()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.txtStart = New System.Windows.Forms.TextBox()
+        Me.txtEnd = New System.Windows.Forms.TextBox()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblStart
+        '
+        Me.lblStart.AutoSize = True
+        Me.lblStart.Location = New System.Drawing.Point(29, 11)
+        Me.lblStart.Name = "lblStart"
+        Me.lblStart.Size = New System.Drawing.Size(110, 13)
+        Me.lblStart.TabIndex = 0
+        Me.lblStart.Text = "Enter starting number:"
+        '
+        'lblEnd
+        '
+        Me.lblEnd.AutoSize = True
+        Me.lblEnd.Location = New System.Drawing.Point(29, 39)
+        Me.lblEnd.Name = "lblEnd"
+        Me.lblEnd.Size = New System.Drawing.Size(108, 13)
+        Me.lblEnd.TabIndex = 1
+        Me.lblEnd.Text = "Enter ending number:"
+        '
+        'lblResult
+        '
+        Me.lblResult.Location = New System.Drawing.Point(-1, 76)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(283, 23)
+        Me.lblResult.TabIndex = 2
+        Me.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtStart
+        '
+        Me.txtStart.Location = New System.Drawing.Point(156, 8)
+        Me.txtStart.Name = "txtStart"
+        Me.txtStart.Size = New System.Drawing.Size(100, 20)
+        Me.txtStart.TabIndex = 3
+        '
+        'txtEnd
+        '
+        Me.txtEnd.Location = New System.Drawing.Point(156, 36)
+        Me.txtEnd.Name = "txtEnd"
+        Me.txtEnd.Size = New System.Drawing.Size(100, 20)
+        Me.txtEnd.TabIndex = 4
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(78, 107)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(119, 23)
+        Me.btnCalc.TabIndex = 5
+        Me.btnCalc.Text = "Calculate Sum"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 142)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.txtEnd)
+        Me.Controls.Add(Me.txtStart)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.lblEnd)
+        Me.Controls.Add(Me.lblStart)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblStart As Label
+    Friend WithEvents lblEnd As Label
+    Friend WithEvents lblResult As Label
+    Friend WithEvents txtStart As TextBox
+    Friend WithEvents txtEnd As TextBox
+    Friend WithEvents btnCalc As Button
+End Class
---
+++ Prog6_3/Prog6_3/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        lblResult.Text = ""
+        Dim intSum As Integer = 0
+        For intI As Integer = Val(txtStart.Text) To Val(txtEnd.Text)
+            lblResult.Text = lblResult.Text & intI & "+"
+            intSum += intI
+        Next
+        lblResult.Text = lblResult.Text.Substring(0, lblResult.Text.Length() - 1) + "=" & intSum
+    End Sub
+End Class
