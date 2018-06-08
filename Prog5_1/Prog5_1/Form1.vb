f61223f9ef10e34e612b2c1abceb5e261a53f1f8
ContronPAnda <rajtel99@gmail.com>
2018-06-08 12:41:17 -0400

    Finished Prog5_1

--- Prog5_1/Prog5_1/Form1.vb
+++ Prog5_1/Prog5_1/Form1.vb
@@ -1,5 +1,11 @@
 ﻿Public Class Form1
     Private Sub btnCheck_Click(sender As Object, e As EventArgs) Handles btnCheck.Click
-        If (Val(txtNum) < 1 OrElse ) Then
+        If Val(txtNum.Text) < 1 OrElse Val(txtNum.Text) > 99 Then
+            lblAnswer.Text = "Invalid number"
+        ElseIf Val(txtNum.Text) < 10 Then
+            lblAnswer.Text = "One digit"
+        Else
+            lblAnswer.Text = "Two digits"
+        End If
     End Sub
 End Class
