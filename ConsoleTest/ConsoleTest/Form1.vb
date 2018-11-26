3cef3c032e0f4f45a7e52f082cd11d651e9f81f0
ContronPAnda <rajtel99@gmail.com>
2018-11-26 10:56:30 -0500

    Improved ConsoleTest

--- ConsoleTest/ConsoleTest/Form1.vb
+++ ConsoleTest/ConsoleTest/Form1.vb
@@ -1,15 +1,35 @@
 ﻿Public Class Form1
+    Private Shared processOutput As String
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         'Runs a sample JAR which saves the location where it's executing from to a text file
-        RunJar("TestJava.jar")
+        Label1.Text = RunJar("TestJava.jar")
     End Sub
-    Shared Sub RunJar(command As String, Optional arguments As String = "")
+    Private Shared Function RunJar(command As String, Optional arguments As String = "") As String
         Dim p As Process = New Process()
-        Dim pi As ProcessStartInfo = New ProcessStartInfo()
-        pi.Arguments = " -jar " + command + " " + arguments
         'javaw instead of java so that a console window does not pop up
-        pi.FileName = "javaw.exe"
-        p.StartInfo = pi
+        With p.StartInfo
+            .FileName = "javaw.exe"
+            .Arguments = " -jar " + command + " " + arguments
+            .RedirectStandardOutput = True
+            .RedirectStandardError = True
+            .RedirectStandardInput = True
+            .UseShellExecute = False
+            .WindowStyle = ProcessWindowStyle.Normal
+            .CreateNoWindow = False
+        End With
+
+        AddHandler p.OutputDataReceived, AddressOf OutputHandler
+
         p.Start()
+        p.BeginOutputReadLine()
+        p.WaitForExit()
+        Return processOutput
+    End Function
+    Private Shared Sub OutputHandler(sendingProcess As Object, outLine As DataReceivedEventArgs)
+        ' Collect the sort command output.
+        If Not String.IsNullOrEmpty(outLine.Data) Then
+            ' Add the text to the collected output.
+            processOutput = processOutput + outLine.Data
+        End If
     End Sub
 End Class
