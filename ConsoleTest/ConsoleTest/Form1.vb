aff7fea581249f8292bd66729bbdc2d937d891fd
ContronPAnda <rajtel99@gmail.com>
2018-11-27 15:48:25 -0500

    Started trying to integrate actual java project

--- ConsoleTest/ConsoleTest/Form1.vb
+++ ConsoleTest/ConsoleTest/Form1.vb
@@ -1,35 +1,6 @@
 ﻿Public Class Form1
-    Private Shared processOutput As String
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         'Runs a sample JAR which saves the location where it's executing from to a text file
-        Label1.Text = RunJar("TestJava.jar")
-    End Sub
-    Private Shared Function RunJar(command As String, Optional arguments As String = "") As String
-        Dim p As Process = New Process()
-        'javaw instead of java so that a console window does not pop up
-        With p.StartInfo
-            .FileName = "javaw.exe"
-            .Arguments = " -jar " + command + " " + arguments
-            .RedirectStandardOutput = True
-            .RedirectStandardError = True
-            .RedirectStandardInput = True
-            .UseShellExecute = False
-            .WindowStyle = ProcessWindowStyle.Normal
-            .CreateNoWindow = False
-        End With
-
-        AddHandler p.OutputDataReceived, AddressOf OutputHandler
-
-        p.Start()
-        p.BeginOutputReadLine()
-        p.WaitForExit()
-        Return processOutput
-    End Function
-    Private Shared Sub OutputHandler(sendingProcess As Object, outLine As DataReceivedEventArgs)
-        ' Collect the sort command output.
-        If Not String.IsNullOrEmpty(outLine.Data) Then
-            ' Add the text to the collected output.
-            processOutput = processOutput + outLine.Data
-        End If
+        Label1.Text = JarRunner.RunJar("TestJava.jar", "null staticPrint")
     End Sub
 End Class
---
+++ ConsoleTest/ConsoleTest/JarRunner.vb
@@ -0,0 +1,31 @@
+﻿Public Class JarRunner
+    Private Shared processOutput As String
+    Public Shared Function RunJar(command As String, Optional arguments As String = "") As String
+        Dim p As Process = New Process()
+        'javaw instead of java so that a console window does not pop up
+        With p.StartInfo
+            .FileName = "javaw.exe"
+            .Arguments = " -jar " + command + " " + arguments
+            .RedirectStandardOutput = True
+            .RedirectStandardError = True
+            .RedirectStandardInput = True
+            .UseShellExecute = False
+            .WindowStyle = ProcessWindowStyle.Normal
+            .CreateNoWindow = False
+        End With
+
+        AddHandler p.OutputDataReceived, AddressOf OutputHandler
+
+        p.Start()
+        p.BeginOutputReadLine()
+        p.WaitForExit()
+        Return processOutput
+    End Function
+    Private Shared Sub OutputHandler(sendingProcess As Object, outLine As DataReceivedEventArgs)
+        ' Collect the sort command output.
+        If Not String.IsNullOrEmpty(outLine.Data) Then
+            ' Add the text to the collected output.
+            processOutput = processOutput + outLine.Data
+        End If
+    End Sub
+End Class
